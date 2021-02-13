72e6710f1e124e778e0ab12c4e273d4693f1a07d
Levon Ghukasyan <levon_ghukasyan@live.com>
2021-02-13 09:42:50 +0400

    added adjecency

---
+++ include/adjacency.h
@@ -0,0 +1,33 @@
+#paragma once
+
+namespace graph {
+
+template <typename T = size_t>
+class Adjacency
+{
+public:
+    using object_t = T;
+
+    Adjacenc(const vertexIdx = T(), const edgeIdx);
+
+    object_t getVertex() const;
+    object_t& getVertex();
+
+    object_t getEdge() const;
+    object_t getEdge();
+
+    bool operator < (const Adjacency<object_t>& other) const
+    bool operator <= (const Adjacency<object_t>& other) const
+
+    bool operator > (const Adjacency<object_t>& other) const
+    bool operator <= (const Adjacency<object_t>& other) const
+
+    bool operator == (const Adjacency<object_t>& other) const
+    bool operator != (const Adjacency<object_t>& other) const
+
+private:
+    object_t m_vertex;
+    object_t m_edge;
+};
+
+}
